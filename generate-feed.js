61c8d3a85ea08ab7ef60f666c2d755d746a5a86a
Dusty <info@stof.me>
2025-01-19 09:09:21 +0100

    Changed description of the podcast

--- generate-feed.js
+++ generate-feed.js
@@ -5,7 +5,7 @@ import fs from 'fs/promises';
 const CONFIG = {
     pageUrl: 'https://robstenders.nl/podcast/9/index',  // <- Change this!
     feedTitle: 'De Bonanza', // <- Change this!
-    feedDescription: 'De Bonanza met Rob Stenders is elke werkdag van 14:00 tot 16:00 uur te horen op Radio Veronica. Jij bepaalt welke muziek er wordt gedraaid. Vraag jouw favoriete platen aan via de gratis Radio Veronica app.', // <- Change this!
+    feedDescription: 'De Bonanza met Rob Stenders is elke werkdag van 14:00 tot 16:00 uur te horen op Radio Veronica. Dit is geen officiële podcast feed van Veronica of Rob Stenders. De feed wordt automatisch gegenereerd op basis van informatie op de website van Rob Stenders.', // <- Change this!
     feedAuthor: 'Rob Stenders', // <- Change this!
     feedLanguage: 'nl-nl',
     feedImage: 'https://pbs.twimg.com/profile_images/646042764493373441/q3Cw3a5y.png'  // Optional: Add URL to podcast artwork
